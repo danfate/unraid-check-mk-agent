f6b2913f34d39bcb9ede54862b120b1cd8e0eebf
Maximilian Donhauser <35191173+Donimax@users.noreply.github.com>
2023-07-10 16:15:36 +0200

    Add build pipelines via github action

--- source/compile_docker.sh
+++ source/compile_docker.sh
@@ -10,6 +10,7 @@ fi
 # Define additional variables
 DEB_NAME="$(ls -la ${DATA_DIR}/*.deb | cut -d '/' -f4)"
 LAT_V="$(echo "$DEB_NAME" | cut -d '_' -f2 | cut -d '-' -f1)"
+VERSION="$(echo "$DEB_NAME" | sed 's/[^0-9]*\([0-9]\+\)\.\([0-9]\+\).*/\1\2/')"
 
 # Install needed Slackware packages
 slackpkg -batch=on -default_answer=y install flex binutils
@@ -27,10 +28,10 @@ cd ${DATA_DIR}/extracted
 mkdir -p ${DATA_DIR}/$LAT_V
 
 # Create Slackware package
-makepkg -l y -c y ${DATA_DIR}/$LAT_V/check_mk_agent-"$(date +'%Y.%m.%d')".tgz
+makepkg -l y -c y ${DATA_DIR}/$LAT_V/check_mk_agent-"$(date +'%Y.%m.%d')".$VERSION.tgz
 cd ${DATA_DIR}/$LAT_V
-md5sum check_mk_agent-"$(date +'%Y.%m.%d')".tgz > check_mk_agent-"$(date +'%Y.%m.%d')".tgz.md5
+md5sum check_mk_agent-"$(date +'%Y.%m.%d')".$VERSION.tgz > check_mk_agent-"$(date +'%Y.%m.%d')".$VERSION.tgz.md5
 
 # Move to packages folder and cleanup
-mv check_mk_agent-"$(date +'%Y.%m.%d')".tgz check_mk_agent-"$(date +'%Y.%m.%d')".tgz.md5 /build/packages
+mv check_mk_agent-"$(date +'%Y.%m.%d')".$VERSION.tgz check_mk_agent-"$(date +'%Y.%m.%d')".$VERSION.tgz.md5 /build/packages
 rm -rf ${DATA_DIR}/*
